234359f45cbf8215bc58491138a0b4be3cd92ef4
Adam Bollinger <adampbollinger@gmail.com>
2020-08-21 18:48:41 -0400

    Removed comment

--- angular-client/src/app/app.module.ts
+++ angular-client/src/app/app.module.ts
@@ -9,11 +9,10 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { AddMemberComponent } from './add-member/add-member.component';
 import { MemberFormComponent } from './member-form/member-form.component';
+import { LoadingOverlayComponent } from './loading-overlay/loading-overlay.component';
 
 // To remove the errors complaining that $ and .modal are undefined
-///import * as $ from "jquery";
 import * as bootstrap from "bootstrap";
-import { LoadingOverlayComponent } from './loading-overlay/loading-overlay.component';
 
 @NgModule({
   declarations: [		
